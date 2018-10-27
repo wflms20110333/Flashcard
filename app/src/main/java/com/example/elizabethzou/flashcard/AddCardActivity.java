fa19ee428ff6b455a1a6320111aa42701236ff2d
Elizabeth Zou <elizabeth.zou.0@gmail.com>
2018-10-27 18:39:06 -0400

    done!

--- app/src/main/java/com/example/elizabethzou/flashcard/AddCardActivity.java
+++ app/src/main/java/com/example/elizabethzou/flashcard/AddCardActivity.java
@@ -1,8 +1,11 @@
 package com.example.elizabethzou.flashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -11,10 +14,43 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+        if (getIntent().getBooleanExtra("edit", false)) {
+            String question = getIntent().getStringExtra("question");
+            String correct_answer = getIntent().getStringExtra("correct_answer");
+            String incorrect_answer_1 = getIntent().getStringExtra("incorrect_answer_1");
+            String incorrect_answer_2 = getIntent().getStringExtra("incorrect_answer_2");
+            ((EditText) findViewById(R.id.question)).setText(question);
+            ((EditText) findViewById(R.id.correct_answer)).setText(correct_answer);
+            ((EditText) findViewById(R.id.incorrect_answer_1)).setText(incorrect_answer_1);
+            ((EditText) findViewById(R.id.incorrect_answer_2)).setText(incorrect_answer_2);
+        }
+
         findViewById(R.id.cancel_card).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setResult(RESULT_CANCELED);
                 finish();
+            }
+        });
+
+        findViewById(R.id.save_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = ((EditText) findViewById(R.id.question)).getText().toString();
+                String correct_answer = ((EditText) findViewById(R.id.correct_answer)).getText().toString();
+                String incorrect_answer_1 = ((EditText) findViewById(R.id.incorrect_answer_1)).getText().toString();
+                String incorrect_answer_2 = ((EditText) findViewById(R.id.incorrect_answer_2)).getText().toString();
+                if (question.length() == 0 || correct_answer.length() == 0 || incorrect_answer_1.length() == 0 || incorrect_answer_2.length() == 0)
+                    Toast.makeText(getApplicationContext(), "Must enter all question and answer choices!", Toast.LENGTH_SHORT).show();
+                else {
+                    Intent data = new Intent();
+                    data.putExtra("question", question);
+                    data.putExtra("correct_answer", correct_answer);
+                    data.putExtra("incorrect_answer_1", incorrect_answer_1);
+                    data.putExtra("incorrect_answer_2", incorrect_answer_2);
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/example/elizabethzou/flashcard/MainActivity.java
+++ app/src/main/java/com/example/elizabethzou/flashcard/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.elizabethzou.flashcard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,8 +66,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivity(intent);
+                MainActivity.this.startActivityForResult(intent, 0);
             }
         });
+
+        findViewById(R.id.edit_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("edit", true);
+                intent.putExtra("question", ((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                intent.putExtra("correct_answer", ((TextView) findViewById(R.id.flashcard_answer)).getText().toString());
+                intent.putExtra("incorrect_answer_1", ((TextView) findViewById(R.id.incorrect_answer_1)).getText().toString());
+                intent.putExtra("incorrect_answer_2", ((TextView) findViewById(R.id.incorrect_answer_2)).getText().toString());
+                MainActivity.this.startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String question = data.getExtras().getString("question");
+                String correct_answer = data.getExtras().getString("correct_answer");
+                String incorrect_answer_1 = data.getExtras().getString("incorrect_answer_1");
+                String incorrect_answer_2 = data.getExtras().getString("incorrect_answer_2");
+                ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(correct_answer);
+                ((TextView) findViewById(R.id.incorrect_answer_1)).setText(incorrect_answer_1);
+                ((TextView) findViewById(R.id.incorrect_answer_2)).setText(incorrect_answer_2);
+                ((TextView) findViewById(R.id.correct_answer)).setText(correct_answer);
+                Snackbar.make(findViewById(R.id.rootView), "Card successfully created!", Snackbar.LENGTH_LONG).show();
+            }
+            findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            findViewById(R.id.incorrect_answer_1).setBackground(getResources().getDrawable(R.drawable.choice_background));
+            findViewById(R.id.incorrect_answer_2).setBackground(getResources().getDrawable(R.drawable.choice_background));
+            findViewById(R.id.correct_answer).setBackground(getResources().getDrawable(R.drawable.choice_background));
+        }
     }
 }
